0dccf916b3ab129bc49a59f2365481a7ed41527d
Devansh-Balani8 <100129107+Devansh-Balani8@users.noreply.github.com>
2022-04-26 19:43:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+var intro="My first console project"
+var firstname="Devansh"
+var lastname="Balani"
+var fullname=firstname.concat(lastname)
+console.log(fullname)
+var age_decrement=9
+age_decrement--
+console.log(age_decrement)
+var weight_increment=21
+weight_increment++
+console.log(weight_increment)
+var grandfather_age=Math.max(40,10,36,82,62,8)
+console.log(grandfather_age)
+var my_age_min=(40,10,36,82,62,8)
+console.log(my_age_min)
